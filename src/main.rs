9eb00c73c7a34512bc0fbbb71d66e86f4b84d00a
Pierre Baconnier <Pierre.Baconnier@direct-energie.com>
2020-11-30 17:18:45 +0100

    Add necessary dependancies to Cargo.toml and validate the build with libraries usage. Add some snippets, will use later

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+//#[macro_use] extern crate serde_derive;
+use serde::{Serialize, Deserialize};
+
+//use rocket::State;
+//use rocket_contrib::json::{Json, JsonValue};
+
+//#[post("/user", format = "application/json", data = "<user>")]
+//fn new_user(user: User) { /* ... */ }
+
+#[get("/")]
+fn index() -> &'static str {
+    ""
+}
+
+
+//#[derive(Deserialize)]
+//struct Task {
+//    description: String,
+//    complete: bool
+//}
+//
+//#[post("/todo", data = "<task>")]
+//fn new(task: Json<Task>) { /* .. */ }
+
+//#[get("/<id>", format = "json")]
+//fn get(id: ID, map: State<MessageMap>) -> Option<Json<Message>> {
+//    let hashmap = map.lock().unwrap();
+//    hashmap.get(&id).map(|contents| {
+//        Json(Message {
+//            id: Some(id),
+//            contents: contents.clone()
+//        })
+//    })
+//}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index]).launch();
 }
